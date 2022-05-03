6ab67670bb47d2c6e541708a3d35f2dce7bd55f8
Cyan <cyankindasus@protonmail.com>
2022-05-03 17:21:29 +0800

    forced darkmode checker done

--- js/app.js
+++ js/app.js
@@ -13,7 +13,33 @@
 window.onload = function () {
 	// delay so that website fully loads
 	// if we do not delay the alert will stop the page from loading
-	setTimeout(function () {
-		alert("Website is WIP!!!");
-	}, 1000);
+	setTimeout(init, 1000);
 };
+
+function init() {
+	alert("Website is WIP!!!");
+
+	// if the function exists
+	// this part is used to check if the user is using forced darkmode
+	if (window.getComputedStyle) {
+		try {
+			var el = document.createElement("div");
+			var style = el.style;
+			style.display = "none";
+			style.backgroundColor = "canvas";
+			style.colorScheme = "light";
+			el.innerText = "hewo";
+			document.documentElement.appendChild(el);
+			if (getComputedStyle(el).backgroundColor != "rgb(255, 255, 255)") {
+				alert(
+					"please disable force dark mode for this website\ni know you hate light mode but this website is already in dark mode.  There should be instructions on your browser's help page on how to disable it for a specific site. If not, then RIP"
+				);
+				// forced darkmode mega cringe
+			}
+			// support IE
+			el.parentNode.removeChild(el);
+		} catch (e) {
+			console.error("forced dark mode check failed", e);
+		}
+	}
+}
