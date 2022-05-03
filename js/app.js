8279f1c85406ebbad13292a7ace7b649b4f90914
Cyan <cyankindasus@protonmail.com>
2022-05-04 01:04:01 +0800

    fonts other stuff and other stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+function redirectToLegacy() {
+	var url = "https://karstenph.github.io/legacy/";
+	try {
+		// if the user was redirected before, let's not force the user lol
+		if (!localStorage.getItem("redirected")) {
+			localStorage.setItem("redirected", true);
+			url = window.location.href + "/legacy/";
+			window.location.href = "/legacy";
+		}
+	} catch (e) {
+		// if JS redirect doesn't work, we show popup
+		alert("please use the legacy version of this website!\n" + url);
+	}
+}
+
+// this tests if everything works fine, if not redirect to legacy
+// if the user was redirected before, we will not force it...
+if (!localStorage.getItem("redirected")) {
+	try {
+		var string = "this is a test...";
+		localStorage.setItem("test", string);
+		var test = localStorage.getItem("test");
+		if (test != string) throw Error("localStorage doesn't work...");
+		if (!window.location && !window.location.href) throw Error("no href!");
+		ready();
+	} catch (e) {
+		redirectToLegacy(e);
+	}
+}
+
 // Element.prototype.children returns DOMList which is not an Array
 // Arrays have a forEach method/function but DOMList does not
 // alternative way of Array.from is [...document.body.children] aka spread
@@ -6,40 +36,47 @@
 // 	a.style.setProperty("--delay", (i + 1) / 10 + 0.5 + "s");
 // });
 
-// we use this for browser support
-// notice how i did not use an arrow function (()=>{})
-// or used addEventListener, this is because onload is very old
-// arrow functions started existing in 2015/2016
-window.onload = function () {
-	// delay so that website fully loads
-	// if we do not delay the alert will stop the page from loading
-	setTimeout(init, 1000);
-};
+function ready() {
+	try {
+		// we use this for browser support
+		// notice how i did not use an arrow function (()=>{})
+		// or used addEventListener, this is because onload is very old
+		// arrow functions started existing in 2015/2016
+		window.onload = function () {
+			// delay so that website fully loads
+			// if we do not delay the alert will stop the page from loading
+			setTimeout(init, 1000);
+		};
 
-function init() {
-	alert("Website is WIP!!!");
+		function init() {
+			alert("Website is WIP!!!");
 
-	// if the function exists
-	// this part is used to check if the user is using forced darkmode
-	if (window.getComputedStyle) {
-		try {
-			var el = document.createElement("div");
-			var style = el.style;
-			style.display = "none";
-			style.backgroundColor = "canvas";
-			style.colorScheme = "light";
-			el.innerText = "hewo";
-			document.documentElement.appendChild(el);
-			if (getComputedStyle(el).backgroundColor != "rgb(255, 255, 255)") {
-				alert(
-					"please disable force dark mode for this website\ni know you hate light mode but this website is already in dark mode.  There should be instructions on your browser's help page on how to disable it for a specific site. If not, then RIP"
-				);
-				// forced darkmode mega cringe
+			// this part is used to check if the user is using forced darkmode
+			// if the function exists
+			if (window.getComputedStyle) {
+				try {
+					var el = document.createElement("div");
+					var style = el.style;
+					style.display = "none";
+					style.backgroundColor = "canvas";
+					style.colorScheme = "light";
+					el.innerText = "hewo";
+					document.documentElement.appendChild(el);
+					if (getComputedStyle(el).backgroundColor != "rgb(255, 255, 255)") {
+						alert(
+							"please disable force dark mode for this website\ni know you hate light mode but this website is already in dark mode.  There should be instructions on your browser's help page on how to disable it for a specific site. If not, then RIP"
+						);
+						// forced darkmode mega cringe
+					}
+					// support IE
+					el.parentNode.removeChild(el);
+				} catch (e) {
+					console.error("forced dark mode check failed", e);
+				}
 			}
-			// support IE
-			el.parentNode.removeChild(el);
-		} catch (e) {
-			console.error("forced dark mode check failed", e);
 		}
+	} catch (e) {
+		// if the functions on top fail, we redirect
+		redirectToLegacy();
 	}
 }
