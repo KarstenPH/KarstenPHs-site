61031c95dde66eea0dec0d73a8533cdad4364c5b
Cyan <cyankindasus@protonmail.com>
2022-05-03 15:48:43 +0800

    test 404 page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-Array.from(document.body.children).forEach((a, i) => {
-	a.style.animationFillMode = "forwards";
-	a.style.animation = `swoosh-left 0.5s ease ${(i + 1) / 10 + 0.5}s forwards`;
-});
+// Element.prototype.children returns DOMList which is not an Array
+// Arrays have a forEach method/function but DOMList does not
+// alternative way of Array.from is [...document.body.children] aka spread
+// no longer needed, wrote it directly to the html file
+// Array.from(document.body.children).forEach((a, i) => {
+// 	a.style.setProperty("--delay", (i + 1) / 10 + 0.5 + "s");
+// });
+
+alert("Website is WIP!!!");
